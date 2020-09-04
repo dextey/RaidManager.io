69d8a1b70bf9364d29ff8cc35f74821b050fe464
Łukasz Pindel <lukasz.pindel1996@gmail.com>
2020-09-04 20:17:04 +0200

    Added login angular template form to navbar

--- RaidManagement-App/src/app/navbar/navbar.component.ts
+++ RaidManagement-App/src/app/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  model: any = {};
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log(this.model);
+  }
 }
