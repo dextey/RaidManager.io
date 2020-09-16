336bcc8307b1fa1ce04a09fc969cc9e27a2fe20d
Łukasz Pindel <lukasz.pindel1996@gmail.com>
2020-09-16 14:47:29 +0200

    Used angular jwt to decode JWT tokens stored in localStorage. LoggedIn method now is based on expiry date of the token

--- RaidManagement-App/src/app/services/auth.service.ts
+++ RaidManagement-App/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -7,22 +8,31 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
 
-constructor(private http: HttpClient) { }
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-login(model: any) {
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(
       map((response: any) => {
         const user = response;
         if(user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     )
-}
+  }
 
-register(model: any) {
+  register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
-}
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
