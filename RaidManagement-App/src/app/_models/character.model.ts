40a24b3e34aa9c2d174f8d07a3cda9e78208c4b5
Łukasz Pindel <lukasz.pindel1996@gmail.com>
2020-09-28 14:50:28 +0200

    Added character model class

---
+++ RaidManagement-App/src/app/_models/character.model.ts
@@ -0,0 +1,13 @@
+
+export class Character {
+    name: string;
+    race: string;
+    class: string;
+    activeSpecName: string;
+    activeSpecRole: string;
+    gender: string;
+    faction: string;
+    region: string;
+    realm: string;
+    profileUrl: string;
+}
