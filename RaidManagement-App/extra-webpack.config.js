e2d4543edb603a16642933cd3427b714017e38c7
Łukasz Pindel <lukasz.pindel1996@gmail.com>
2020-11-13 14:20:02 +0100

    Added dotenv config

--- RaidManagement-App/extra-webpack.config.js
+++ RaidManagement-App/extra-webpack.config.js
@@ -2,6 +2,14 @@ const Dotenv = require('dotenv-webpack');
  
 module.exports = {
   plugins: [
-    new Dotenv()
+    new Dotenv({
+      path: './.env',
+      safe: false,
+      allowEmptyValues: false,
+      systemvars: false,
+      silent: false,
+      expand: false,
+      defaults: false
+    })
   ]
 };
