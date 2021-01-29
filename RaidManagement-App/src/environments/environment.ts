0acff90f5eb43827c2d7bce895c470d91eb54709
Lukasz Pindel <lukasz.pindel1996@gmail.com>
2021-01-29 13:52:24 +0100

    Removed customwebpack, dotenv webpack, installed dotenv, removed previous custom webpack configuration, added .env keys to environment.ts

--- RaidManagement-App/src/environments/environment.ts
+++ RaidManagement-App/src/environments/environment.ts
@@ -1,9 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+require('dotenv').config();
 
 export const environment = {
-  production: false
+  production: false,
+  CLIENT_ID: process.env.ID,
+  CLIENT_SECRET: process.env.SECRET
 };
 
 /*
