f215eccdcf948270cae2709a25d2a73eddd811ae
Lukasz Pindel <lukasz.pindel1996@gmail.com>
2021-03-03 13:10:48 +0100

    Created config script to populate environment.ts with keys from .env file, added the script to run before serve / build. Added href to authorize button on edit-profile page

--- RaidManagement-App/src/app/edit-profile/edit-profile.component.ts
+++ RaidManagement-App/src/app/edit-profile/edit-profile.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
+  client_id: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.client_id = environment.CLIENTID;
   }
 
 }
