13051f373eec2105487b230255c871cac0074369
Lukasz Pindel <lukasz.pindel1996@gmail.com>
2021-03-05 11:45:13 +0100

    Generated a service responsible for getting access token via oauth2, added post request

---
+++ RaidManagement-App/src/app/services/oauth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OauthService {
+  baseUrl = "https://eu.battle.net/oauth/token?grant_type=client_credentials";
+  region: string;
+  authData = 'Basic' + btoa(environment.CLIENTID + ':' + environment.CLIENTSECRET);
+  oauthHttpOptions = {
+    headers: new HttpHeaders( {
+      'Content-Type' : 'application/json',
+      'Authorization' : this.authData
+    })
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  acquireAccessToken(model: any) {
+    return this.http.post(this.baseUrl, null, this.oauthHttpOptions)
+  }
+}
